acb2e38b1a831c634e1d310f81fe7ac69adbc942
Jordan Burton <jordieburton3@gmail.com>
2019-07-21 00:26:35 -0400

    Added more styling

--- src/Carousel/Carousel.tsx
+++ src/Carousel/Carousel.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
+import $ from 'jquery';
 import "../Styles/AppStyles.css";
 import { AdditionalCarouselClasses } from '../Constants/Classes';
 
 const images = ["http://www.grcrunning.com/uploads/5/6/0/9/5609246/elena-clarendonday2_orig.jpg", "http://www.grcrunning.com/uploads/5/6/0/9/5609246/gw-parkway-grc_orig.jpg"]
 
 export const Carousel: React.FunctionComponent = () => {
-    const [imageToUse, setImageToUse] = React.useState(0);
-    const imageToUseRef = React.useRef(imageToUse);
+	const [imageToUse, setImageToUse] = React.useState(0);
+	const [mediaStyles, setMediaHeight] = React.useState({});
+	const imageToUseRef = React.useRef(imageToUse);
+	const mediaStylesRef = React.useRef(mediaStyles);
     imageToUseRef.current = imageToUse;
     React.useEffect(() => {
         const timer = setInterval(() => {
@@ -14,9 +17,15 @@ export const Carousel: React.FunctionComponent = () => {
         }, 2000);
         return () => clearInterval(timer);
       }, []);
+    React.useEffect(() => {
+		const height = $(".carousel__image")[0].scrollHeight;
+		setMediaHeight({ ...mediaStylesRef, height: `${height + 48}px` });
+	});
     return (
-        <div className="carousel__wrapper">
-            {images.map((image, i) => <img alt="" src={image} key={image} className={`carousel__image ${imageToUse === i ? "" : AdditionalCarouselClasses.HIDDEN}`}/>)}
+        <div className="carousel__wrapper" style={mediaStyles}>
+			{images.map((image, i) => <img alt="" src={image} key={image} className={`carousel__image ${imageToUse === i ? "" : AdditionalCarouselClasses.HIDDEN}`}/>)}
+			<div className="carousel__title">GEORGETOWN</div>
+			<div className="carousel__subtitle">RUNNING CLUB</div>
         </div>
     );
 }
